de063e548861b4c794a2cb597d718fe02ce76f8c
FrozeyNight <frozeynight@gmail.com>
2025-12-24 22:44:33 +0100

    Learned about compileCommands, the :: operator and how to avoid having to loop through all controls to find the desired ones

---
+++ ToDoListProgram/include/MainFrame.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <wx/wx.h>
+
+class MainFrame : public wxFrame{
+public:
+    MainFrame(const wxString& title);
+private:
+    void CreateControls();
+    void BindEventHandlers();
+
+    void OnAddButtonClicked(wxCommandEvent& evt);
+
+    void AddTaskFromInput();
+
+    wxPanel* panel;
+    wxStaticText* headlineText;
+    wxTextCtrl* inputField;
+    wxButton* addButton;
+    wxCheckListBox* checkListBox;
+    wxButton* clearButton;
+};
